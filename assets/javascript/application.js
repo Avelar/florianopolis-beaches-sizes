5952e65547a3ffd8013e2d85c02531be2ed0c2b9
avelar <fortunato.avelar@gmail.com>
2018-09-03 15:54:44 -0300

    improve better style

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,17 +1,28 @@
-var map = L.map("map").setView([-20.22, -42.55], 11);
-// https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYXZlbGFyIiwiYSI6ImNpZzVxd29hbjRkdXB1dm0zNWdoeDVrYjQifQ.NswKzz5O8mJ7yZunhaGTag
+// Set map position
+var map = L.map("map").setView([-40.22, -60.55], 11);
+// Mapbox
+// https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}
 
+// Open Street Map
+// http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+
+// Get openstretmap with leaflet
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 12,
     minZoom: 10,
     attribution: 'Map data by <a href="http://www.avelarfortunato.com">Avelar Fortunato</a> contributors, <a href="http://openstreetmap.org">openstreetmap</a>',
-    // id: 'avelar.o00bh9gb'
+    // id: 'avelar.o00bh9gb',
+    // accessToken: 'pk.eyJ1IjoiYXZlbGFyIiwiYSI6ImNpZzVxd29hbjRkdXB1dm0zNWdoeDVrYjQifQ.NswKzz5O8mJ7yZunhaGTag'
 }).addTo(map);
 
+// disable duble click
 map.doubleClickZoom.disable();
+//
 L.control.scale().addTo(map);
 
+// set empty array
 var beachs = [];
+// get data.json
 $.getJSON('data.json', function(json) {
   beachs = json;
   getBeachs(beachs);
@@ -19,6 +30,7 @@ $.getJSON('data.json', function(json) {
 
 getBeachs(beachs);
 
+// get data and use maker to populate map
 function getBeachs(array) {
   html = "";
   var i;
@@ -36,6 +48,7 @@ function getBeachs(array) {
     $('#detail-list-container .tbody').append("<tr><td>" + array[i].beachName + "</td><td>" + array[i].lengthBeach + "</td><td>"  + array[i].region + "</td></tr>");
 }
 
+// get data beach on select
 $("#beachOptions").on("change", function(){
   var coordinatesList = $("#beachOptions").val();
   for (i = 0; i < beachs.length; i++) {
@@ -55,9 +68,14 @@ $("#beachOptions").on("change", function(){
   }
 });
 
+//
   $(".nav-pin").on("click", function(){
     var mip = $(".map-info-pin");
         mip.addClass("map-info-colapsed");
         mip.removeClass("map-info");
+  });
+
+  $("button.about-btn").click(function(){
+    $(".about").fadeIn( "slow", "linear" );
   });
 }
